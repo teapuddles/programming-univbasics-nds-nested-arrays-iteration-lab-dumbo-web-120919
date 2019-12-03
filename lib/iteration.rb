6a29419dbfc2ce50fe5f5a3bc8803ace6c658c03
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-03 22:25:41 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -31,7 +31,7 @@ def total_even_pairs(src)
   row_index = 0 
   while row_index < src.count do
     if src[row_index][0][1] % 2 == 0
-    total += row_index[0][1]
+    total += src[row_index][0] + src[row_index][1]
   end
     row_index += 1
   end
